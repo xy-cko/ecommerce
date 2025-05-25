24f01bd89b8199cf9674ec8b7e2da145ece0d21b
xy-cko <95402398+xy-cko@users.noreply.github.com>
2025-05-25 04:48:23 +0200

    oko

--- src/main/java/com/company/ecommerce/view/product/MyProductListView.java
+++ src/main/java/com/company/ecommerce/view/product/MyProductListView.java
@@ -1,17 +1,15 @@
 package com.company.ecommerce.view.product;
 
+import com.company.ecommerce.entity.Acceptence;
 import com.company.ecommerce.entity.Product;
-import com.company.ecommerce.entity.User;
-import com.company.ecommerce.repository.ProductRepository;
 import com.company.ecommerce.view.main.MainView;
-import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.grid.DataGrid;
-import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,4 +20,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 @LookupComponent("productsDataGrid")
 @DialogMode(width = "64em")
 public class MyProductListView extends StandardListView<Product> {
+    @ViewComponent
+    private DataGrid<Product> productsDataGrid;
+
+    @Autowired
+    private DataManager dataManager;
+
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe(id = "editButton", subject = "clickListener")
+    public void onEditButtonClick(final ClickEvent<JmixButton> event) {
+        Product editedItem = productsDataGrid.getSingleSelectedItem();
+        if (editedItem != null) {
+            editedItem.setIsAccepted(Acceptence.WAITING_FOR_APPROVAL);
+            dataManager.save(editedItem);
+        }
+
+    }
+
 }
